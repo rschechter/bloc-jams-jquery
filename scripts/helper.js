efa0af41bd27046bcfe8df9e202e64df63d276f6
Rachelli Schechter <rachelschechter@nat-tvwna-inside-visitornet101-b-534.princeton.org>
2018-07-19 08:59:09 -0400

    new helper file for total duration

---
+++ scripts/helper.js
@@ -0,0 +1,10 @@
+class Helper {
+
+  playPauseAndUpdate(song){
+    player.playPause(song);
+
+  const duration = player.currentlyPlaying.duration;
+    $('#time-control .total-time').text(duration);
+  }
+}
+const helper = new Helper();
